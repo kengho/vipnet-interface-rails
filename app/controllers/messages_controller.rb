c4e6e932fde7449bbaf657e3e70f881064fccbbf
Alexander Morozov <ntcomp12@gmail.com>
2016-03-26 17:53:44 -0400

    fixed mistake in messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
   def index
-	p Settings.support_email
-#    @messages = Message.all.paginate(page: params[:page], per_page: 50)
+    @messages = Message.all.paginate(page: params[:page], per_page: 50)
   end
 end
