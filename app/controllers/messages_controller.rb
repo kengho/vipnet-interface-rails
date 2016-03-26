6bd73d1da9af32a24716e1adabb6b8ed1eba1ac8
user <user@ubuntu.rk.local>
2016-03-26 14:50:04 -0400

    clean messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,5 @@
 class MessagesController < ApplicationController
   def index
-
-# p User.all
-# user = User.find(1)
-# p user.valid_password?("Admin778")
-# user.methods.sort.each { |m| p m }
-# user = User.new(  email: "test4@test.ru",password: "Admin777", password_confirmation: "Admin777")
-# p user.save
-# p user
-    # session = UserSession.create(:email => "admin@admin.com", :password => "Admin777")
-    # session.save
-    # p session
-    # session2 = UserSession.find
-    # session2 = UserSession.find(:email => "admin@admin.com")
-# UserSession.methods.sort.each { |m| p m }
-# p session2
-    # @messages = Message.all.paginate(page: params[:page], per_page: 50)
+    @messages = Message.all.paginate(page: params[:page], per_page: 50)
   end
 end
