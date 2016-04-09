947963a1333ef77b71b34a6b32b9539850c846d4
Alexander Morozov <ntcomp12@gmail.com>
2016-04-09 16:58:59 -0400

    added tests for MessagesController

---
+++ test/controllers/messages_controller_test.rb
@@ -0,0 +1,13 @@
+class MessagesControllerTest < ActionController::TestCase
+  test "available by administrator role" do
+    user_session1 = UserSession.create(users(:administrator))
+    get :index
+    assert_response :success
+  end
+
+  test "unavailable by user role" do
+    user_session1 = UserSession.create(users(:user1))
+    get :index
+    assert_response :redirect
+  end
+end
