3571d3de15392f86aa3d64b5dc4a285e3c4cbbb4
Alexander Morozov <ntcomp12@gmail.com>
2016-04-09 16:21:23 -0400

    added tests for SettingsController

--- test/controllers/nodes_controller_test.rb
+++ test/controllers/nodes_controller_test.rb
@@ -2,16 +2,19 @@ class NodesControllerTest < ActionController::TestCase
 
   test "available by user role" do
     user_session1 = UserSession.create(users(:user1))
-
     get :index
     assert_response :success
+  end
+
+  test "check params" do
+    user_session1 = UserSession.create(users(:user1))
 
     get :availability, node_id: nodes(:empty_node)
     assert_response :success
 
     get :availability, node_id: "node dont exist"
     assert_response :bad_request
-    
+
     get :history, node_id: nodes(:empty_node)
     assert_response :success
 
