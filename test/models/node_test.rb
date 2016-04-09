c6495678c947c7ab0495c1824f6e6f511d8ff4b8
Alexander Morozov <ntcomp12@gmail.com>
2016-04-09 09:08:39 -0400

    added tests for Node

--- test/models/node_test.rb
+++ test/models/node_test.rb
@@ -1,9 +1,6 @@
 require "test_helper"
 
 class NodesTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
 
   test "presence" do
     node1 = Node.new(vipnet_id: nil, network_id: "network_id", name: "name")
@@ -15,31 +12,31 @@ class NodesTest < ActiveSupport::TestCase
   end
 
   test "normalize_vipnet_id" do
-    vipnet_id1 = Node.normalize_vipnet_id("1A0E1234")
+    vipnet_id1 = Node.normalize_vipnet_id("1A0E0100")
     vipnet_id2 = Node.normalize_vipnet_id("not looks like id at all")
-    assert_equal("0x1a0e1234", vipnet_id1)
+    assert_equal("0x1a0e0100", vipnet_id1)
     assert_equal(false, vipnet_id2)
   end
 
   test "network" do
-    network1 = Node.network("0x1a0e1234")
+    network1 = Node.network("0x1a0e0100")
     network2 = Node.network("not looks like id at all")
     assert_equal("6670", network1)
     assert_equal(false, network2)
   end
 
   test "accessips" do
-    node1 = Node.new(vipnet_id: "0x1a0e1234")
-    node2 = Node.new(vipnet_id: "0x1a0effff")
+    node1 = Node.new(vipnet_id: "0x1a0e0100")
+    node2 = nodes(:empty_node)
     assert_equal(["192.0.2.1", "192.0.2.2"], node1.accessips)
     assert_equal([], node2.accessips)
   end
 
   test "availability" do
-    node1 = Node.new(vipnet_id: "0x1a0e1234")
-    node2 = Node.new(vipnet_id: "0x1a0effff")
+    node1 = Node.new(vipnet_id: "0x1a0e0200")
+    node2 = nodes(:empty_node)
     assert_equal({ :data => { :availability => true }}, node1.availability)
-    assert_equal({ :errors => [ { :title => "internal", :detail => "no accessips" }]}, node2.availability)
+    assert_equal({ :errors => [{ :title => "internal", :detail => "no accessips" }]}, node2.availability)
   end
 
   test "ips_summary" do
