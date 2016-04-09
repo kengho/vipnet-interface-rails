ea988ae7f68e3b5ba4fd1979160d4f9944d98fb8
Alexander Morozov <ntcomp12@gmail.com>
2016-04-09 10:16:18 -0400

    added tests for Iplirconf

--- test/models/iplirconf_test.rb
+++ test/models/iplirconf_test.rb
@@ -2,17 +2,41 @@ require "test_helper"
 
 class IplirconfsTest < ActiveSupport::TestCase
 
-  # test "presence" do
-  #   network = Network.new
-  #   network.save(:validate => false)
-  #   coordinator1 = Coordinator.new(vipnet_id: nil, network_id: network.id)
-  #   coordinator2 = Coordinator.new(vipnet_id: "vipnet_id", network_id: nil)
-  #   coordinator3 = Coordinator.new(vipnet_id: "same_vipnet_id", network_id: network.id)
-  #   coordinator4 = Coordinator.new(vipnet_id: "same_vipnet_id", network_id: network.id)
-  #   assert_not coordinator1.save
-  #   assert_not coordinator2.save
-  #   coordinator3.save
-  #   assert_not coordinator4.save
-  # end
+  test "presence" do
+    coordinator = Coordinator.new
+    coordinator.save(:validate => false)
+    iplirconf1 = Iplirconf.new(coordinator_id: nil)
+    iplirconf2 = Iplirconf.new(coordinator_id: coordinator.id)
+    assert_not iplirconf1.save
+    assert iplirconf2.save
+  end
+
+  test "parse" do
+    iplirconf1 =iplirconfs(:iplirconf_parse1)
+    assert iplirconf1.parse
+    assert iplirconf1.sections["self"]
+    assert_equal("0x1a0e030a", iplirconf1.sections["self"]["vipnet_id"])
+    # 3 sections: "self", "0x1a0e030a" and "0x1a0e0301"
+    assert_equal(3, iplirconf1.sections.size)
+    
+    iplirconf1.sections.each do |_, section|
+      assert_instance_of(Hash, section)
+      case section["vipnet_id"]
+      when "0x1a0e030a"
+        assert_equal("coordinator", section["name"])
+        assert_equal(["192.0.2.1", "192.0.2.2", "192.0.2.3"], section["ips"])
+        assert_equal("192.0.2.2", section["accessip"])
+        assert_equal("3.0-670", section["vipnet_version"])
+      when "0x1a0e0301"
+        assert_equal("client1", section["name"])
+        assert_equal(["192.0.2.11"], section["ips"])
+        assert_equal("192.0.2.12", section["accessip"])
+        assert_equal("3.2-672", section["vipnet_version"])
+      end
+    end
+
+    iplirconf2 =iplirconfs(:iplirconf_parse2)
+    assert_not iplirconf2.parse
+  end
 
 end
