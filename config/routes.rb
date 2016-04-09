51a7339562c4230847ec151d270fc11c7141c30b
Alexander Morozov <ntcomp12@gmail.com>
2016-04-09 17:02:26 -0400

    deleted nodenames_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,12 @@ Rails.application.routes.draw do
 
   # # close registration
   resources :users, only: [:update, :edit]
-  # resources :users, only: [:edit]
   # http://stackoverflow.com/questions/5136940/undefined-method-user-path
   # match '/users/:id/edit', :to => 'users#edit', :as => :user, :via => :get
   resources :user_sessions, only: [:create, :destroy]
 
   delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
   get '/sign_in', to: 'user_sessions#new', as: :sign_in
-
-  # # close registration
-  # get "users/sign_up" => redirect("/404.html")
-  # # devise_for :users
-  # # https://github.com/plataformatec/devise/wiki/How-To:-Disable-user-from-destroying-their-account
-  # devise_for :users, skip: :registrations
-  # devise_scope :user do
-  #   resource :registration,
-  #     only: [:new, :create, :edit, :update],
-  #     path: 'users',
-  #     path_names: { new: 'sign_up' },
-  #     controller: 'devise/registrations',
-  #     as: :user_registration do
-  #       get :cancel
-  #     end
-  # end
 
   match 'users/:id' => 'users#destroy', :via => :delete
   match 'users/:id' => 'users#update', :via => :get
