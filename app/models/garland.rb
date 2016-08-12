0a8f9721c508fd303530f825b0b1c039b5d1c485
Alexander Morozov <ntcomp12@gmail.com>
2016-08-12 09:41:55 +0300

    added belongs_to_id to garland

--- app/models/garland.rb
+++ app/models/garland.rb
@@ -5,58 +5,73 @@ class Garland < ActiveRecord::Base
 
   SNAPSHOT = false
   DIFF = true
+  NEXT_ID_PENDING = -1
 
   def self.push(args)
-    # args processing
+
     if args.class != Hash
       return false
     end
     if args[:hash]
       h = args[:hash]
       b_to = args[:belongs_to]
+      b_to_id = b_to.id
     else
       h = args
+      b_to = nil
+      b_to_id = nil
     end
 
-    # main operations
-    if self.all.size == 0
-      n = self.new(entity: h.to_s, entity_type: SNAPSHOT)
+    thread = self.thread(b_to_id)
+    if thread.size == 0
+      n = self.new(entity: h.to_s, entity_type: SNAPSHOT, belongs_to_id: b_to_id)
+      return n.save
     else
-      s = self.snapshot
-      last_e = self.find_by(next: nil)
+      s = self.snapshot(b_to_id)
+      last_e = thread.find_by(next: nil)
+      last_e.next = NEXT_ID_PENDING
+      unless last_e.save
+        return false
+      end
       d = HashDiffSym.diff(eval(s.entity), h)
-      n = self.new(entity: d.to_s, entity_type: DIFF, previous: last_e.id)
-    end
+      n = self.new(entity: d.to_s, entity_type: DIFF, previous: last_e.id, belongs_to_id: b_to_id)
 
-    # ending
-    if b_to
-      attr_name = b_to.class.name.downcase
-      n.public_send("#{attr_name}=", b_to)
-    end
-    if last_e
       if n.save
         last_e.next = n.id
-        return last_e.save
+        if last_e.save
+          return true
+        else
+          n.destroy
+          return false
+        end
       else
         return false
       end
-    else
-      return n.save
     end
   end
 
-  def self.snapshot
-    last_s = self.find_by(next: nil)
+  def self.snapshot(b_to_id = nil)
+    thread = self.thread(b_to_id)
+    last_s = thread.find_by(next: nil)
     until last_s.entity_type == SNAPSHOT do
-      last_s = self.find_by_id(last_s.previous)
+      last_s = thread.find_by_id(last_s.previous)
     end
     s = last_s
     s_h = eval(s.entity)
     until s.next == nil do
-      n = self.find_by_id(s.next)
+      n = thread.find_by_id(s.next)
       s_h = HashDiffSym.patch!(s_h, eval(n.entity))
       s = n
     end
     self.new(entity: s_h.to_s, entity_type: SNAPSHOT)
   end
+
+  private
+    def self.thread(b_to_id)
+      if b_to_id == nil
+        return self.where("belongs_to_id is NULL")
+      else
+        return self.where("belongs_to_id = ?", b_to_id)
+      end
+    end
 end
