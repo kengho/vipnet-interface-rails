6f65f68b23626e549ec2fe8984d38e7abd635dcc
Alexander Morozov <ntcomp12@gmail.com>
2016-08-20 18:19:44 +0300

    added self.to_json_for(table_type)

--- app/models/abstract_model.rb
+++ app/models/abstract_model.rb
@@ -16,4 +16,12 @@ class AbstractModel < ActiveRecord::Base
   def to_json_for(table_type)
     self.to_json(:only => table_type.constantize.props_from_file + [:vid]).gsub("null", "nil")
   end
+
+  def self.to_json_for(table_type)
+    result = []
+    self.all.each do |e|
+      result.push(eval(e.to_json_for(table_type)))
+    end
+    result.to_json.gsub("null", "nil")
+  end
 end
