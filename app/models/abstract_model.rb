295ac52cb097a21a014cd46068e3b90398d8d623
Alexander Morozov <ntcomp12@gmail.com>
2016-08-20 18:14:13 +0300

    added to_json_for(table_type) (for tests)

--- app/models/abstract_model.rb
+++ app/models/abstract_model.rb
@@ -2,7 +2,7 @@ class AbstractModel < ActiveRecord::Base
   self.abstract_class = true
 
   def to_json_nonmagic
-    self.to_json(:except => [:created_at, :updated_at]).gsub("null", "nil")
+    self.to_json(:except => [:id, :created_at, :updated_at]).gsub("null", "nil")
   end
 
   def self.to_json_nonmagic
@@ -11,5 +11,9 @@ class AbstractModel < ActiveRecord::Base
       result.push(eval(e.to_json_nonmagic))
     end
     result.to_json.gsub("null", "nil")
+  end
+
+  def to_json_for(table_type)
+    self.to_json(:only => table_type.constantize.props_from_file + [:vid]).gsub("null", "nil")
   end
 end
--- app/models/nodename.rb
+++ app/models/nodename.rb
@@ -1,41 +1,8 @@
 class Nodename < Garland
   belongs_to :network, foreign_key: "belongs_to_id"
-  validates :belongs_to_id, presence: true,
-                            uniqueness: true
+  validates :belongs_to_id, presence: true
 
-  def changed_records(new_nodename)
-    # make hashes out of nested hstore
-    new_nodename_records = Hash.new
-    new_nodename.records.each do |id, record|
-      new_nodename_records[id] = eval(record)
-    end
-    self_records = Hash.new
-    self.records.each do |id, record|
-      self_records[id] = eval(record)
-    end
-
-    changed_records = Hash.new
-    diffs = HashDiff.diff(self_records, new_nodename_records)
-    diffs.each do |diff|
-      change = diff[0]
-      if change == "+"
-        id = VipnetParser::id(diff[1])[0]
-        changed_records[id] = diff[2]
-      elsif change == "~"
-        diff[1] =~ /(.*)\.(.*)/
-        id = VipnetParser::id(Regexp.last_match(1))[0]
-        prop_name = Regexp.last_match(2)
-        new_value = diff[3]
-        changed_records[id] = Hash.new unless changed_records[id]
-        changed_records[id][prop_name.to_sym] = new_value
-      elsif change == "-"
-        # do nothing
-      end
-    end
-    changed_records
-  end
-
-  def self.props_from_record
+  def self.props_from_file
     [:name, :enabled, :category, :abonent_number, :server_number]
   end
 end
