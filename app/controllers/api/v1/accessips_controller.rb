438736c10435e4cd3253b6ae54252d60d8751292
Alexander Morozov <ntcomp12@gmail.com>
2016-08-22 00:39:08 +0300

    changed accessip api

--- app/controllers/api/v1/accessips_controller.rb
+++ app/controllers/api/v1/accessips_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::AccessipsController < Api::V1::BaseController
   def index
     @response = Hash.new
     if params[:accessip]
-      nodes = Node.none
-      vipnet_ids = Array.new
-      only = params[:only] || ["vipnet_id", "enabled"]
-      iplirconfs = Iplirconf.all
+      nodes = CurrentNode.none
+      # vids = Array.new
+      # only = params[:only] || ["vipnet_id", "enabled"]
+      # iplirconfs = Iplirconf.all
     else
       @response[:errors] = [{
         title: "external",
@@ -19,21 +19,17 @@ class Api::V1::AccessipsController < Api::V1::BaseController
       render json: @response and return
     end
 
-    iplirconfs.each do |iplirconf|
-      iplirconf.sections.each do |_, section|
-        vipnet_ids.push(eval(section)["vipnet_id"]) if eval(section)["accessip"] == params[:accessip]
-        if eval(section)["accessip"] == params[:accessip]
-          new_nodes = Node.where("vipnet_id = ? AND history = 'false' AND deleted_at is null", eval(section)["vipnet_id"])
-          nodes = nodes | new_nodes
-        end
-      end
+    Coordinator.all.each do |coord|
+      new_nodes = CurrentNode.where("accessip -> '#{coord.vid}' = ?", params[:accessip])
+      nodes = nodes | new_nodes
     end
+
     if nodes.size == 0
       @response[:errors] = [{ title: "external", detail: "Node not found" }]
       render json: @response and return
     elsif nodes.size == 1
       node = nodes.first
-      @response[:data] = @response.merge(node.attributes.slice(*only))
+      @response[:data] = @response.merge({ vid: node.vid })
       render json: @response and return
     elsif nodes.size > 1
       @response[:errors] = [{
@@ -44,5 +40,31 @@ class Api::V1::AccessipsController < Api::V1::BaseController
       }]
       render json: @response and return
     end
+
+    # iplirconfs.each do |iplirconf|
+    #   iplirconf.sections.each do |_, section|
+    #     vipnet_ids.push(eval(section)["vipnet_id"]) if eval(section)["accessip"] == params[:accessip]
+    #     if eval(section)["accessip"] == params[:accessip]
+    #       new_nodes = Node.where("vipnet_id = ? AND history = 'false' AND deleted_at is null", eval(section)["vipnet_id"])
+    #       nodes = nodes | new_nodes
+    #     end
+    #   end
+    # end
+    # if nodes.size == 0
+    #   @response[:errors] = [{ title: "external", detail: "Node not found" }]
+    #   render json: @response and return
+    # elsif nodes.size == 1
+    #   node = nodes.first
+    #   @response[:data] = @response.merge(node.attributes.slice(*only))
+    #   render json: @response and return
+    # elsif nodes.size > 1
+    #   @response[:errors] = [{
+    #     title: "internal",
+    #     detail: "Please report to developer. "\
+    #             "Multiple nodes found. "\
+    #             "Params: #{params.except(:token)}"
+    #   }]
+    #   render json: @response and return
+    # end
   end
 end
--- test/controllers/api/v1/accessips_controller_test.rb
+++ test/controllers/api/v1/accessips_controller_test.rb
@@ -1,79 +1,59 @@
 require "test_helper"
 
 class Api::V1::AccessipsControllerTest < ActionController::TestCase
-  test "accessips" do
-    Node.new(
-      vipnet_id: "0x1a0e0001",
-      name: "client1",
-      network_id: networks(:network1).id,
-      enabled: true,
-      history: false,
-    ).save!
-    Node.new(
-      vipnet_id: "0x1a0e0002",
-      name: "client2",
-      network_id: networks(:network1).id,
-      enabled: true,
-      history: false,
-    ).save!
-    Node.new(
-      vipnet_id: "0x1a0e0003",
-      name: "client3",
-      network_id: networks(:network1).id,
-      enabled: true,
-      history: false,
-    ).save!
-    Iplirconf.new(
-      coordinator_id: coordinators(:coordinator1).id,
-      sections: {
-        "self" => {
-          "vipnet_id" => "0x1a0e000a",
-        },
-        "client1" => {
-          "vipnet_id" => "0x1a0e0001",
-          "accessip" => "192.0.2.1",
-        },
-        "client2" => {
-          "vipnet_id" => "0x1a0e0003",
-          "accessip" => "192.0.2.3",
-        },
+  setup do
+    Coordinator.new(vid: "0x1a0e000a").save(validate: false)
+    Coordinator.new(vid: "0x1a0e000d").save(validate: false)
+
+    CurrentNode.new(
+      vid: "0x1a0e0001",
+      :accessip => {
+        "0x1a0e000a" => "198.51.100.1",
+        "0x1a0e000d" => "203.0.113.1",
       },
-    ).save!
-    Iplirconf.new(
-      coordinator_id: coordinators(:coordinator2).id,
-      sections: {
-        "self" => {
-          "vipnet_id" => "0x1a0e000b",
-        },
-        "client1" => {
-          "vipnet_id" => "0x1a0e0001",
-          "accessip" => "192.0.2.2",
-        },
-        "client2" => {
-          "vipnet_id" => "0x1a0e0002",
-          "accessip" => "192.0.2.3",
-        },
+    ).save(validate: false)
+    CurrentNode.new(
+      vid: "0x1a0e0002",
+      :accessip => {
+        "0x1a0e000a" => "198.51.100.2",
+        "0x1a0e000d" => "203.0.113.3",
       },
-    ).save!
+    ).save(validate: false)
+    CurrentNode.new(
+      vid: "0x1a0e0003",
+      :accessip => {
+        "0x1a0e000a" => "198.51.100.3",
+        "0x1a0e000d" => "203.0.113.3",
+      },
+    ).save(validate: false)
+  end
 
-    get(:index, { accessip: "192.0.2.1", token: "GET_INFORMATION_TOKEN" })
-    assert_equal({ data: { "vipnet_id" => "0x1a0e0001", "enabled" => true }}, assigns["response"])
-
-    get(:index, { accessip: "unmatched ip", token: "GET_INFORMATION_TOKEN" })
-    assert assigns["response"][:errors]
-    assert_equal("external", assigns["response"][:errors][0][:title])
-
-    # multiple nodes found
-    get(:index, { accessip: "192.0.2.3", token: "GET_INFORMATION_TOKEN" })
-    assert assigns["response"][:errors]
-    assert_equal("internal", assigns["response"][:errors][0][:title])
-
-    get(:index, { not_accessip_param: "true", token: "GET_INFORMATION_TOKEN" })
+  test "should return error if accessip not provided" do
+    get(:index, { accessip: nil, token: "GET_INFORMATION_TOKEN" })
     assert assigns["response"][:errors]
     assert_equal("external", assigns["response"][:errors][0][:title])
     assert_routing(assigns["response"][:errors][0][:links][:related][:href], { controller: "api/v1/doc", action: "index" })
+  end
 
-    get(:index, { token: "not a valid token" })
+  test "should return error when no valid token provided" do
+    get(:index, { token: "incorrect token" })
     assert_response :unauthorized
+  end
+
+  test "should return vid by accessip" do
+    get(:index, { accessip: "198.51.100.1", token: "GET_INFORMATION_TOKEN" })
+    assert_equal({ data: { vid: "0x1a0e0001" }}, assigns["response"])
+  end
+
+  test "should return error if accessip not found" do
+    get(:index, { accessip: "0.0.0.0", token: "GET_INFORMATION_TOKEN" })
+    assert assigns["response"][:errors]
+    assert_equal("external", assigns["response"][:errors][0][:title])
+  end
+
+  test "should return error if multiple nodes found" do
+    get(:index, { accessip: "203.0.113.3", token: "GET_INFORMATION_TOKEN" })
+    assert assigns["response"][:errors]
+    assert_equal("internal", assigns["response"][:errors][0][:title])
   end
 end
