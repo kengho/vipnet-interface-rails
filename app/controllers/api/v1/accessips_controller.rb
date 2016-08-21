09acf445aad7cd50a0c76dc070782f14f375f6e4
Alexander Morozov <ntcomp12@gmail.com>
2016-08-22 00:46:14 +0300

    removed comments

--- app/controllers/api/v1/accessips_controller.rb
+++ app/controllers/api/v1/accessips_controller.rb
@@ -3,9 +3,6 @@ class Api::V1::AccessipsController < Api::V1::BaseController
     @response = Hash.new
     if params[:accessip]
       nodes = CurrentNode.none
-      # vids = Array.new
-      # only = params[:only] || ["vipnet_id", "enabled"]
-      # iplirconfs = Iplirconf.all
     else
       @response[:errors] = [{
         title: "external",
@@ -40,31 +37,5 @@ class Api::V1::AccessipsController < Api::V1::BaseController
       }]
       render json: @response and return
     end
-
-    # iplirconfs.each do |iplirconf|
-    #   iplirconf.sections.each do |_, section|
-    #     vipnet_ids.push(eval(section)["vipnet_id"]) if eval(section)["accessip"] == params[:accessip]
-    #     if eval(section)["accessip"] == params[:accessip]
-    #       new_nodes = Node.where("vipnet_id = ? AND history = 'false' AND deleted_at is null", eval(section)["vipnet_id"])
-    #       nodes = nodes | new_nodes
-    #     end
-    #   end
-    # end
-    # if nodes.size == 0
-    #   @response[:errors] = [{ title: "external", detail: "Node not found" }]
-    #   render json: @response and return
-    # elsif nodes.size == 1
-    #   node = nodes.first
-    #   @response[:data] = @response.merge(node.attributes.slice(*only))
-    #   render json: @response and return
-    # elsif nodes.size > 1
-    #   @response[:errors] = [{
-    #     title: "internal",
-    #     detail: "Please report to developer. "\
-    #             "Multiple nodes found. "\
-    #             "Params: #{params.except(:token)}"
-    #   }]
-    #   render json: @response and return
-    # end
   end
 end
