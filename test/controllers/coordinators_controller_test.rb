438a3b77132cd175b4a670ea4d6a9e13455f5aae
Alexander Morozov <ntcomp12@gmail.com>
2016-08-21 12:56:31 +0300

    made little fix

--- test/controllers/coordinators_controller_test.rb
+++ test/controllers/coordinators_controller_test.rb
@@ -1,11 +1,11 @@
 class CoordinatorsControllerTest < ActionController::TestCase
-  test "available by administrator role" do
+  test "shold be available by administrator role" do
     user_session1 = UserSession.create(users(:administrator))
     get :index
     assert_response :success
   end
 
-  test "unavailable by user role" do
+  test "shold be unavailable by user role" do
     user_session1 = UserSession.create(users(:user1))
     get :index
     assert_response :redirect
--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -1,11 +1,11 @@
 class SettingsControllerTest < ActionController::TestCase
-  test "available by administrator role" do
+  test "should be available by administrator role" do
     user_session1 = UserSession.create(users(:administrator))
     get :index
     assert_response :success
   end
 
-  test "unavailable by user role" do
+  test "should be unavailable by user role" do
     user_session1 = UserSession.create(users(:user1))
     get :index
     assert_response :redirect
