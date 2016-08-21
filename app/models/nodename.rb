3d33d794cbc274619c54980007ccc1d8ee4d5ad8
Alexander Morozov <ntcomp12@gmail.com>
2016-08-21 13:34:27 +0300

    added validations for Nodename

--- app/models/nodename.rb
+++ app/models/nodename.rb
@@ -1,6 +1,7 @@
 class Nodename < Garland
   belongs_to :network, foreign_key: "belongs_to_id"
-  validates :belongs_to_id, presence: true
+  validates :network, presence: true
+  validates_inclusion_of :belongs_to_type, in: ["Network"]
 
   def self.props_from_file
     [:name, :enabled, :category, :abonent_number, :server_number]
--- test/models/nodename_test.rb
+++ test/models/nodename_test.rb
@@ -1,111 +1,16 @@
 require "test_helper"
 
 class NodenamesTest < ActiveSupport::TestCase
-  test "validations" do
-    nodename1 = Nodename.new(network_id: nil)
-    assert_not nodename1.save
+  test "should not save without network" do
+    n = Nodename.new(entity: "{}", entity_type: Garland::SNAPSHOT)
+    assert_not n.save
   end
 
-  test "changed_records" do
-    nodename1 = Nodename.new(network_id: networks(:network1).id)
-    nodename1.save!
-    initial_records = {
-      "1A0E000B" => {
-        :name => "administrator",
-        :enabled => true,
-        :category => :client,
-        :server_number => "0001",
-        :abonent_number => "0001",
-        :id => "1A0E000B",
-      },
-      "1A0E000A" => {
-        :name => "coordinator1",
-        :enabled => true,
-        :category => :server,
-        :server_number => "0001",
-        :abonent_number => "0000",
-        :id => "1A0E000A",
-      },
-      "1A0E0000" => {
-        :name => "Вся сеть",
-        :enabled => true,
-        :category => :group,
-        :server_number => "0000",
-        :abonent_number => "0000",
-        :id => "1A0E0000",
-      },
-    }
-    nodename2 = Nodename.new(network_id: networks(:network1).id)
-    nodename2.records = initial_records
-
-    initial_records_normalized = Hash.new
-    initial_records.each do |id, record|
-      normalized_id = VipnetParser::id(id)[0]
-      initial_records_normalized[normalized_id] = record
-    end
-
-    assert_equal(initial_records_normalized, nodename1.changed_records(nodename2))
-
-    nodename1.records = initial_records
-    nodename1.save!
-    changed_records1 = {
-      "1A0E000B" => {
-        :name => "administrator_changed",
-        :enabled => true,
-        :category => :client,
-        :server_number => "0001",
-        :abonent_number => "0001",
-        :id => "1A0E000B",
-      },
-      "1A0E000A" => {
-        :name => "coordinator1",
-        :enabled => true,
-        :category => :server,
-        :server_number => "0001",
-        :abonent_number => "0000",
-        :id => "1A0E000A",
-      },
-      "1A0E0000" => {
-        :name => "Вся сеть",
-        :enabled => true,
-        :category => :group,
-        :server_number => "0000",
-        :abonent_number => "0000",
-        :id => "1A0E0000",
-      },
-    }
-    nodename2.records = changed_records1
-
-    assert_equal({ "0x1a0e000b" => { :name => "administrator_changed" }}, nodename1.changed_records(nodename2))
-
-    changed_records2 = {
-      "1A0E000E" => {
-        :name => "coordinator2",
-        :id => "1A0E000E",
-      },
-    }
-    nodename2.records = changed_records2
-
-    assert_equal({
-      "0x1a0e000e" => {
-        :name => "coordinator2",
-        :id => "1A0E000E",
-      },
-    }, nodename1.changed_records(nodename2))
-
-    changed_records3 = {
-      "1A0E000B" => {
-        :server_number => "0002",
-        :abonent_number => "0002",
-      },
-    }
-    nodename2.records = changed_records3
-
-    assert_equal({
-      "0x1a0e000b" => {
-        :server_number => "0002",
-        :abonent_number => "0002",
-      },
-    }, nodename1.changed_records(nodename2))
+  test "should not save if belongs_to_type isn't 'Network'" do
+    @network1 = networks(:network1)
+    Nodename.push(hash: "{}", belongs_to: @network1)
+    n = Nodename.first
+    n.belongs_to_type = "Something"
+    assert_not n.save
   end
 end
