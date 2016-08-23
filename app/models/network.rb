fce4ca00573f9eb36d8dd98f4420ec59b7a7f68c
Alexander Morozov <ntcomp12@gmail.com>
2016-08-23 22:23:39 +0300

    remade Network tests and fixed network_vid validation

--- app/models/network.rb
+++ app/models/network.rb
@@ -2,7 +2,10 @@ class Network < ActiveRecord::Base
   has_many :nodenames, dependent: :destroy, foreign_key: "belongs_to_id"
   has_many :coordinators, dependent: :destroy
   has_many :nodes, dependent: :destroy
-  validates :network_vid, presence: true,
-                          uniqueness: true,
-                          format: { with: /\A[0-9]{4}\z/, message: "network_vid should be like \"6670\"" }
+  validates :network_vid, presence: true, uniqueness: true
+  validates_each :network_vid do |record, attr, value|
+    unless value.to_i >= 0x1 && value.to_i <= 0xffff
+      record.errors.add(attr, "network_vid should be between 1 and 65535")
+    end
+  end
 end
--- test/models/network_test.rb
+++ test/models/network_test.rb
@@ -1,14 +1,30 @@
 require "test_helper"
 
 class NetworksTest < ActiveSupport::TestCase
-  test "validations" do
-    network1 = Network.new(vipnet_network_id: nil)
-    network2 = Network.new(vipnet_network_id: "6671")
-    network2.save
-    network3 = Network.new(vipnet_network_id: "6671")
-    network4 = Network.new(vipnet_network_id: "network")
-    assert_not network1.save
-    assert_not network3.save
-    assert_not network4.save
+  setup do
+    Network.destroy_all
+  end
+
+  test "shouldn't save without network_vid" do
+    network = Network.new()
+    assert_not network.save
+  end
+
+  test "shouldn't save two networks with same network_vid" do
+    Network.create!(network_vid: "6670")
+    network = Network.new(network_vid: "6670")
+    assert_not network.save
+  end
+
+  test "shouldn't save network with wrong network_vid" do
+    network = Network.new(network_vid: "0x1a0e0001")
+    assert_not network.save
+  end
+
+  test "should save network with network_vid in range 0x1-0xffff" do
+    network1 = Network.new(network_vid: "0xffff".to_i(16).to_s)
+    network2 = Network.new(network_vid: "1")
+    assert network1.save
+    assert network2.save
   end
 end
