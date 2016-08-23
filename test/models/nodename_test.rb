809400f2b0e312e147cfe52408f10d69022d9ee0
Alexander Morozov <ntcomp12@gmail.com>
2016-08-23 22:26:43 +0300

    made Nodename tests

--- test/models/nodename_test.rb
+++ test/models/nodename_test.rb
@@ -1,4 +1,19 @@
 require "test_helper"
 
 class NodenamesTest < ActiveSupport::TestCase
+  setup do
+    @network1 = networks(:network1)
+  end
+
+  test "shouldn't save without network" do
+    nodename = Nodename.new()
+    assert_not nodename.save
+  end
+
+  test "when network destroys, all its nodenames destroys" do
+    Nodename.push(hash: {}, belongs_to: @network1)
+    assert_equal(1, Nodename.all.size)
+    @network1.destroy
+    assert_equal(0, Nodename.all.size)
+  end
 end
