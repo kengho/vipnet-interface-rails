841afb6dea5b28a550c4d31389e6a4179426f7db
Alexander Morozov <ntcomp12@gmail.com>
2016-09-20 23:26:17 +0300

    fixed node api

--- app/controllers/api/v1/nodes_controller.rb
+++ app/controllers/api/v1/nodes_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::NodesController < Api::V1::BaseController
     @response = Hash.new
     if params[:vid]
       only = params[:only] || ["name"]
-      node = CurrentNode.find_by(vid: params[:vid])
+      ncc_node = CurrentNccNode.find_by(vid: params[:vid])
     else
       @response[:errors] = [{
         title: "external",
@@ -30,12 +30,38 @@ class Api::V1::NodesController < Api::V1::BaseController
       render json: @response and return
     end
 
-    if node
+    if ncc_node
+      @response[:data] = {}
       avaliable_fileds = [
         "name", "ip", "accessip", "version", "version_decoded",
         "enabled", "category", "creation_date", "creation_date_accuracy",
       ]
-      @response[:data] = node.attributes.slice(*only & avaliable_fileds)
+      only_filtered = only & avaliable_fileds
+      only_filtered_ncc = only_filtered & NccNode.props_from_nodename.map { |e| e.to_s }
+      only_filtered_hw = only_filtered & (HwNode.props_from_iplirconf + [:version_decoded]).map { |e| e.to_s }
+      if only_filtered_hw.any?
+        ncc_node.hw_nodes.each do |hw_node|
+          only_filtered_hw.each do |param|
+            @response[:data].deep_merge!({
+              param => {
+                hw_node.coordinator.vid => hw_node[param]
+              }
+            })
+          end
+          if only_filtered.include?("ip")
+            ips = []
+            hw_node.node_ips.each { |node_ip| ips.push(IPv4::ip(node_ip.u32)) }
+            if ips.any?
+              @response[:data].deep_merge!({
+                "ip" => {
+                  hw_node.coordinator.vid => ips
+                }
+              })
+            end
+          end
+        end
+      end
+      @response[:data].merge!(ncc_node.attributes.slice(*only_filtered_ncc))
     else
       @response[:errors] = [{ title: "external", detail: "Node not found" }]
     end
--- test/controllers/api/v1/nodes_controller_test.rb
+++ test/controllers/api/v1/nodes_controller_test.rb
@@ -2,31 +2,35 @@ require "test_helper"
 
 class Api::V1::NodesControllerTest < ActionController::TestCase
   setup do
-    CurrentNode.create!(
+    ncc_node = CurrentNccNode.new(
       vid: "0x1a0e0001",
       name: "client-0x1a0e0001",
       network: networks(:network1),
-      ip: {
-        "0x1a0e000a" => "198.51.100.1",
-        "0x1a0e000d" => "198.51.100.1",
-      },
-      accessip: {
-        "0x1a0e000a" => "198.51.100.1",
-        "0x1a0e000d" => "203.0.113.1",
-      },
-      version: {
-        "0x1a0e000a" => "3.0-670",
-        "0x1a0e000d" => "3.0-670",
-      },
-      version_decoded: {
-        "0x1a0e000a" => "3.1",
-        "0x1a0e000d" => "3.1",
-      },
       enabled: true,
       category: "client",
       creation_date: DateTime.now,
       creation_date_accuracy: true,
     )
+    ncc_node.save!
+    hw_node1 = CurrentHwNode.new(
+      ncc_node: ncc_node,
+      coordinator: coordinators(:coordinator1),
+      accessip: "198.51.100.1",
+      version: "0.3-2",
+      version_decoded: HwNode.decode_version("0.3-2"),
+    )
+    hw_node1.save!
+    hw_node2 = CurrentHwNode.new(
+      ncc_node: ncc_node,
+      coordinator: coordinators(:coordinator2),
+      accessip: "198.51.100.2",
+      version: "3.0-670",
+      version_decoded: HwNode.decode_version("3.0-670"),
+    )
+    hw_node2.save!
+    NodeIp.create!(hw_node: hw_node1, u32: IPv4::u32("192.0.2.1"))
+    NodeIp.create!(hw_node: hw_node1, u32: IPv4::u32("192.0.2.2"))
+    NodeIp.create!(hw_node: hw_node2, u32: IPv4::u32("192.0.2.3"))
   end
 
   test "should return error if no valid token provided" do
@@ -38,7 +42,10 @@ class Api::V1::NodesControllerTest < ActionController::TestCase
     get(:index, { token: "GET_INFORMATION_TOKEN" })
     assert assigns["response"][:errors]
     assert_equal("external", assigns["response"][:errors][0][:title])
-    assert_routing(assigns["response"][:errors][0][:links][:related][:href], { controller: "api/v1/doc", action: "index" })
+    assert_routing(
+      assigns["response"][:errors][0][:links][:related][:href],
+      { controller: "api/v1/doc", action: "index" }
+    )
   end
 
   test "should return error if vid not found" do
@@ -53,11 +60,23 @@ class Api::V1::NodesControllerTest < ActionController::TestCase
   end
 
   test "should return information using only" do
-    get(:index, { vid: "0x1a0e0001", only: ["ip", "category", "jibberish"], token: "GET_INFORMATION_TOKEN" })
+    get(:index, {
+      vid: "0x1a0e0001",
+      only: ["ip", "category", "version", "version_decoded", "jibberish"],
+      token: "GET_INFORMATION_TOKEN"
+    })
     assert_equal({ data: {
       "ip" => {
-        "0x1a0e000a" => "198.51.100.1",
-        "0x1a0e000d" => "198.51.100.1",
+        "0x1a0e000a" => ["192.0.2.1", "192.0.2.2"],
+        "0x1a0e000b" => ["192.0.2.3"],
+      },
+      "version" => {
+        "0x1a0e000a" => "0.3-2",
+        "0x1a0e000b" => "3.0-670",
+      },
+      "version_decoded" => {
+        "0x1a0e000a" => HwNode.decode_version("0.3-2"),
+        "0x1a0e000b" => HwNode.decode_version("3.0-670"),
       },
       "category" => "client",
     }}, assigns["response"])
