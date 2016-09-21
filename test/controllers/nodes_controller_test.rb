d1863da825a783d847c099a6a19463c4bb27a068
Alexander Morozov <ntcomp12@gmail.com>
2016-09-21 17:27:27 +0300

    added second quick search test

--- test/controllers/nodes_controller_test.rb
+++ test/controllers/nodes_controller_test.rb
@@ -24,21 +24,21 @@ class NodesControllerTest < ActionController::TestCase
     CurrentNccNode.create!(vid: "0x1a0e0001", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", network: @network)
     get(:index, { vid: "0x1a0e0002" })
-    assert_equal(["0x1a0e0002"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0002"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by abnormal vids" do
     CurrentNccNode.create!(vid: "0x1a0e0001", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", network: @network)
     get(:index, { vid: "1A0E0002" })
-    assert_equal(["0x1a0e0002"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0002"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by part of vid" do
     CurrentNccNode.create!(vid: "0x1a0e0001", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", network: @network)
     get(:index, { vid: "0002" })
-    assert_equal(["0x1a0e0002"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0002"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by range of vids" do
@@ -47,49 +47,49 @@ class NodesControllerTest < ActionController::TestCase
     CurrentNccNode.create!(vid: "0x1a0e0003", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0004", network: @network)
     get(:index, { vid: "0x1a0e0001-0x1a0e0003" })
-    assert_equal(["0x1a0e0001", "0x1a0e0002", "0x1a0e0003"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001", "0x1a0e0002", "0x1a0e0003"], assigns["ncc_nodes"].vids)
   end
 
   test "shouldn't search by range when it's too large" do
     CurrentNccNode.create!(vid: "0x1a0e0001", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0100", network: @network)
     get(:index, { vid: "0x1a0e0001-0x1a0e0100" })
-    assert_equal([], assigns["nodes"].vids)
+    assert_equal([], assigns["ncc_nodes"].vids)
   end
 
   test "should search by name" do
     CurrentNccNode.create!(vid: "0x1a0e0001", name: "Alex", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "John", network: @network)
     get(:index, { name: "Alex" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by partial name" do
     CurrentNccNode.create!(vid: "0x1a0e0001", name: "Alex", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "John", network: @network)
     get(:index, { name: "Al" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by name (case insensitive)" do
     CurrentNccNode.create!(vid: "0x1a0e0001", name: "Alex", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "John", network: @network)
     get(:index, { name: "alex" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search name and treat spaces like anything" do
     CurrentNccNode.create!(vid: "0x1a0e0001", name: "Marcus Forest", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "Wilbur Kelly Mallory", network: @network)
     get(:index, { name: "wil mal" })
-    assert_equal(["0x1a0e0002"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0002"], assigns["ncc_nodes"].vids)
   end
 
   test "should search name using regexp" do
     CurrentNccNode.create!(vid: "0x1a0e0001", name: "Wilbur Kelly Mallory", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "Kelly Wilbur Mallory", network: @network)
     get(:index, { name: "^wilbur\\s" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by ip" do
@@ -100,7 +100,7 @@ class NodesControllerTest < ActionController::TestCase
     NodeIp.create!(hw_node: hw_node1, u32: IPv4::u32("192.168.0.1"))
     NodeIp.create!(hw_node: hw_node2, u32: IPv4::u32("192.168.0.2"))
     get(:index, { ip: "192.168.0.1" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by cidr" do
@@ -114,7 +114,7 @@ class NodesControllerTest < ActionController::TestCase
     NodeIp.create!(hw_node: hw_node2, u32: IPv4::u32("192.168.1.0"))
     NodeIp.create!(hw_node: hw_node3, u32: IPv4::u32("192.168.0.255"))
     get(:index, { ip: "192.168.0.0/24" })
-    assert_equal(["0x1a0e0001", "0x1a0e0003"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001", "0x1a0e0003"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by range" do
@@ -128,12 +128,12 @@ class NodesControllerTest < ActionController::TestCase
     NodeIp.create!(hw_node: hw_node2, u32: IPv4::u32("192.168.0.255"))
     NodeIp.create!(hw_node: hw_node3, u32: IPv4::u32("192.168.0.254"))
     get(:index, { ip: "192.168.0.0-192.168.0.254" })
-    assert_equal(["0x1a0e0001", "0x1a0e0003"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001", "0x1a0e0003"], assigns["ncc_nodes"].vids)
   end
 
   test "shouldn't search by invalid ip" do
     get(:index, { ip: "invalid ip" })
-    assert_equal([], assigns["nodes"].vids)
+    assert_equal([], assigns["ncc_nodes"].vids)
   end
 
   test "should search by version_decoded" do
@@ -160,7 +160,7 @@ class NodesControllerTest < ActionController::TestCase
       version_decoded: "3.2",
     )
     get(:index, { version_decoded: "3.1" })
-    assert_equal(["0x1a0e0002"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0002"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by version_decoded substring" do
@@ -187,7 +187,7 @@ class NodesControllerTest < ActionController::TestCase
       version_decoded: "3.2",
     )
     get(:index, { version_decoded: "3." })
-    assert_equal(["0x1a0e0002"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0002"], assigns["ncc_nodes"].vids)
   end
 
   test "shouldn't treat underscore and percent as special symbols in version_decoded" do
@@ -198,9 +198,9 @@ class NodesControllerTest < ActionController::TestCase
       version_decoded: "3.0",
     )
     get(:index, { version_decoded: "3_" })
-    assert_equal([], assigns["nodes"].vids)
+    assert_equal([], assigns["ncc_nodes"].vids)
     get(:index, { version_decoded: "%3" })
-    assert_equal([], assigns["nodes"].vids)
+    assert_equal([], assigns["ncc_nodes"].vids)
   end
 
   # temporarily implementations of DateTime search
@@ -208,14 +208,14 @@ class NodesControllerTest < ActionController::TestCase
     CurrentNccNode.create!(vid: "0x1a0e0001", creation_date: DateTime.new(2016, 9, 1), network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", creation_date: DateTime.new(2016, 9, 2), network: @network)
     get(:index, { creation_date: "2016-09-01" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by deletion_date (tmp)" do
     CurrentNccNode.create!(vid: "0x1a0e0001", deletion_date: DateTime.new(2016, 9, 1), network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", deletion_date: DateTime.new(2016, 9, 2), network: @network)
     get(:index, { deletion_date: "2016-09-01" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
   # /temporarily implementations of DateTime search
 
@@ -235,7 +235,7 @@ class NodesControllerTest < ActionController::TestCase
       ticket_id: "2",
     )
     get(:index, { ticket: "1" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by ticket substring" do
@@ -254,14 +254,14 @@ class NodesControllerTest < ActionController::TestCase
       ticket_id: "222",
     )
     get(:index, { ticket: "11" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "shouldn't treat empty params as .*" do
     CurrentNccNode.create!(vid: "0x1a0e0001", name: "Alex", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "John", network: @network)
     get(:index, { vid: "0x1a0e0001", name: "" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
   end
 
   test "should search by many params using AND logic" do
@@ -269,13 +269,27 @@ class NodesControllerTest < ActionController::TestCase
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "Alex2", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0010", name: "Alex", network: @network)
     get(:index, { vid: "0x1a0e000", name: "Alex" })
-    assert_equal(["0x1a0e0001", "0x1a0e0002"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001", "0x1a0e0002"], assigns["ncc_nodes"].vids)
   end
 
-  test "should do quick search" do
+  test "should do quick search by name" do
     CurrentNccNode.create!(vid: "0x1a0e0001", name: "Alex", network: @network)
     CurrentNccNode.create!(vid: "0x1a0e0002", name: "John", network: @network)
     get(:index, { search: "0x1a0e0001" })
-    assert_equal(["0x1a0e0001"], assigns["nodes"].vids)
+    assert_equal(["0x1a0e0001"], assigns["ncc_nodes"].vids)
+  end
+
+  test "should do quick search by ip" do
+    ncc_node1 = CurrentNccNode.new(vid: "0x1a0e0001", network: @network); ncc_node1.save!
+    ncc_node2 = CurrentNccNode.new(vid: "0x1a0e0002", network: @network); ncc_node2.save!
+    ncc_node3 = CurrentNccNode.new(vid: "0x1a0e0003", network: @network); ncc_node3.save!
+    hw_node1 = CurrentHwNode.new(ncc_node: ncc_node1, coordinator: @coordinator1); hw_node1.save!
+    hw_node2 = CurrentHwNode.new(ncc_node: ncc_node2, coordinator: @coordinator1); hw_node2.save!
+    hw_node3 = CurrentHwNode.new(ncc_node: ncc_node3, coordinator: @coordinator1); hw_node3.save!
+    NodeIp.create!(hw_node: hw_node1, u32: IPv4::u32("192.168.0.1"))
+    NodeIp.create!(hw_node: hw_node2, u32: IPv4::u32("192.168.0.255"))
+    NodeIp.create!(hw_node: hw_node3, u32: IPv4::u32("192.168.0.254"))
+    get(:index, { search: "192.168.0.1-192.168.0.254" })
+    assert_equal(["0x1a0e0001", "0x1a0e0003"], assigns["ncc_nodes"].vids)
   end
 end
