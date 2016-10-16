674ac8fe937a5e0e309e4ba86f60acea0ef4f2ee
Alexander Morozov <ntcomp12@gmail.com>
2016-10-16 21:01:30 +0300

    fixed models inheritance issues

--- app/models/deleted_hw_node.rb
+++ app/models/deleted_hw_node.rb
@@ -1,2 +1,5 @@
-class DeletedHwNode < CurrentHwNode
+class DeletedHwNode < HwNode
+  validates :coordinator, presence: true
+  validates :ncc_node, presence: true
+  validates_uniqueness_of :ncc_node, scope: [:coordinator]
 end
--- app/models/deleted_ncc_node.rb
+++ app/models/deleted_ncc_node.rb
@@ -1,2 +1,5 @@
-class DeletedNccNode < CurrentNccNode
+class DeletedNccNode < NccNode
+  validates :network, presence: true
+  validates :vid, presence: true
+  validates_uniqueness_of :vid
 end
