4aed58058f27dd836bab9cc1507a43868eb63739
Alexander Morozov <ntcomp12@gmail.com>
2016-11-06 01:42:06 +0300

    fixed application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,34 @@ module ApplicationHelper
     @translations[I18n.locale].with_indifferent_access
   end
 
-  def self.available_locales
-    (I18n.available_locales - [:"zh-CN"] - [:"zh-TW"]).map(&:to_s)
+  def page_title
+    title_locale_path = "#{params[:controller].gsub("/", ".")}.#{params[:action]}.title"
+    title = t(title_locale_path)
+    if title.class == String
+      t(title_locale_path)
+    else
+      t("default.title")
+    end
+  end
+
+  # https://gist.github.com/jeroenr/3142686
+  class LinkRenderer < WillPaginate::ActionView::LinkRenderer
+    def link(text, target, attributes = {})
+      attributes["data-remote"] = true
+      super
+    end
+  end
+
+  def paginate(collection, params = {})
+    will_paginate collection, params.merge(renderer: ApplicationHelper::LinkRenderer)
+  end
+
+  def i(path)
+    icons = YAML.load_file(Rails.root.join("config/icons.yml"))
+    icon = icons.clone
+    path.split(".").each do |turn|
+      icon = icon[turn]
+    end
+    icon
   end
 end
