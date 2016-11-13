5bf2227490c0b4c8befd65871db9488cbfd9a36e
Alexander Morozov <ntcomp12@gmail.com>
2016-11-13 03:18:43 +0300

    added POST_TICKETS_TOKEN to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,7 @@ GET_INFORMATION_TOKEN = SecureRandom.urlsafe_base64(64)
 POST_HW_TOKEN = SecureRandom.urlsafe_base64(64)
 POST_ADMINISTRATOR_TOKEN = SecureRandom.urlsafe_base64(64)
 CHECKER_TOKEN = SecureRandom.urlsafe_base64(64)
+POST_TICKETS_TOKEN = SecureRandom.urlsafe_base64(64)
 
 STDOUT.puts "Please create .env file in root of this app (gem dotenv) or use some other method for storing ENV vars"
 STDOUT.puts ""
@@ -33,6 +34,7 @@ STDOUT.puts "GET_INFORMATION_TOKEN=#{GET_INFORMATION_TOKEN}"
 STDOUT.puts "POST_HW_TOKEN=#{POST_HW_TOKEN}"
 STDOUT.puts "POST_ADMINISTRATOR_TOKEN=#{POST_ADMINISTRATOR_TOKEN}"
 STDOUT.puts "CHECKER_TOKEN=#{CHECKER_TOKEN}"
+STDOUT.puts "POST_TICKETS_TOKEN=#{POST_TICKETS_TOKEN}"
 STDOUT.puts ""
 STDOUT.puts "(You may use your own tokens.)"
 STDOUT.puts ""
