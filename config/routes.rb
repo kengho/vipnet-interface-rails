6024c2b9a66668e2e2c2afa2a3a4b961a3dfccb2
Alexander Morozov <ntcomp12@gmail.com>
2016-12-07 21:52:40 +0300

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
   root "nodes#index"
 
-  # close registration
   resources :users, only: [:update, :edit]
   resources :user_sessions, only: [:create, :destroy]
 
-  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
-  get '/sign_in', to: 'user_sessions#new', as: :sign_in
+  get "sign_in", to: "user_sessions#new", as: :sign_in
+  delete "sign_out", to: "user_sessions#destroy", as: :sign_out
+  get "reset_password", to: "reset_password#index"
 
-  match 'users/:id' => 'users#destroy', :via => :delete
-  match 'users/:id' => 'users#update', :via => :get
+  match "users/:id" => "users#update", via: :get
+  match "users/:id" => "users#destroy", via: :delete
 
-  resources :nodes, only: [:index]
-  get 'nodes/load', to: 'nodes#load'
-  get 'nodes/info', to: 'nodes#info'
-  get 'nodes/history', to: 'nodes#history'
-  get 'nodes/availability', to: 'nodes#availability'
-  get 'reset_password', to: 'reset_password#index'
+  match "settings" => "settings#index", via: :get
+  match "settings" => "settings#update", via: :patch
+
+  get "nodes",              to: "nodes#index"
+  get "nodes/load",         to: "nodes#load"
+  get "nodes/info",         to: "nodes#info"
+  get "nodes/history",      to: "nodes#history"
+  get "nodes/availability", to: "nodes#availability"
 
   namespace :api do
     namespace :v1 do
@@ -31,7 +33,4 @@ Rails.application.routes.draw do
       resources :availability, only: [:index]
     end
   end
-
-  match 'settings' => 'settings#index', :via => :get
-  match 'settings' => 'settings#update', :via => :patch
 end
