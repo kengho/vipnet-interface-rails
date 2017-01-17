f4a242cf08ae010a9b34b901bf5f1727947e0c7b
Alexander Morozov <ntcomp12@gmail.com>
2017-01-17 10:56:48 +0300

    added clear_search_bar button

--- app/assets/javascripts/nodes.js
+++ app/assets/javascripts/nodes.js
@@ -6,3 +6,10 @@ vipnetInterface.nodes = {
     return $("*[data-vid='" + vid + "']");
   },
 };
+
+$(document).ready(function() {
+  $("div[data-clear-search-bar]").click(function() {
+    var $searchBar = $(this).parent().find("input[type=text]");
+    $searchBar.val("");
+  });
+});
